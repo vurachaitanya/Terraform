905bf1f6826c98a584b865f40e2adc885974a80e
Chaitanya Vura <vurachaitanya@users.noreply.github.com>
2021-04-05 18:35:20 +0530

    Create Docker_nginx.tf

---
+++ Docker_nginx.tf
@@ -0,0 +1,23 @@
+### Providers info
+provider "docker" {
+  host = "tcp://docker:2345/"
+}
+
+#### Resources info 
+resource "docker_image" "nginx" {
+  name = "nginx:1.11-alpine"
+}
+
+##### Resources with Nginx images
+resource "docker_container" "nginx-server" {
+  name = "nginx-server"
+  image = "${docker_image.nginx.latest}"
+  ports {
+    internal = 80
+  }
+  volumes {
+    container_path  = "/usr/share/nginx/html"
+    host_path = "/home/scrapbook/tutorial/www"
+    read_only = true
+  }
+}
